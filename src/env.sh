e82676d06267f61cfb856a784ea8ae97ac64edc4
lilinj2000 <lilinj2000@gmail.com>
2016-11-03 09:30:56 +0000

    small update

--- src/env.sh
+++ src/env.sh
@@ -2,7 +2,7 @@
 home_3rd=~/3rd
 
 home_boost=$home_3rd/boost
-home_ctp=$home_3rd/v6.3.6_20141230traderapi_linux64
+home_ctp=$home_3rd/v6.3.6_20160606_api_tradeapi_linux64
 
 export LD_LIBRARY_PATH=$home_boost/lib:$home_ctp
 
